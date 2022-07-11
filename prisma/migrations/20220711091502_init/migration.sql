84919daae41f9a797875f3481015688c7dacbebd
Adam <61081143+Adam1141@users.noreply.github.com>
2022-07-11 12:17:25 +0300

    added prisma, generated an sqlite db with Message table

---
+++ prisma/migrations/20220711091502_init/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "name" TEXT NOT NULL,
+    "message" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "isRead" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" DATETIME NOT NULL
+);
